18b557e16b9b48e21a4cd97e5335bb5f6f0651b7
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2024-12-28 19:54:46 +0100

    finished strings mix

--- strings-mix.py
+++ strings-mix.py
@@ -1,52 +1,51 @@
 def mix(s1, s2):
     s1_count = {}
     s2_count = {}
-    
+
     for c in s1:
         if not c.islower():
             continue
         s1_count[c] = s1_count.get(c, 0) + 1
-        
+
     for c in s2:
         if not c.islower():
             continue
         s2_count[c] = s2_count.get(c, 0) + 1
-    
-    
+
     s1_count = dict(sorted(s1_count.items(), key=lambda item: (-item[1], item[0])))
     s2_count = dict(sorted(s2_count.items(), key=lambda item: (-item[1], item[0])))
-    
-    res = ""
-    
-    print(s1_count)
-    print(s2_count)
-    
+
+    res = {}
+
     for key, value in s1_count.items():
         if not key in s2_count:
             if value == 1:
                 continue
-            res += "/1:" + key * value
+            res[key * value] = 1
         else:
             if value == 1:
-                del s2_count[key]
                 continue
-                
+
             if value < s2_count[key]:
-                res += "/2:" + key * s2_count[key]
+                res[key * s2_count[key]] = 2
             elif value > s2_count[key]:
-                res += "/1:" + key * value
+                res[key * value] = 1
             else:
-                res += "/=:" + key * value
-                
+                res[key * value] = 3
+
             del s2_count[key]
-            
-    print(s2_count)
-    
+
     if len(s2_count) != 0:
         for key, value in s2_count.items():
             if value == 1:
                 continue
-            
-            res += "/2:" + key * value
 
-    return res[1:]
+            res[key * value] = 2
+
+    res = dict(sorted(res.items(), key=lambda item: (-len(item[0]), item[1], item[0])))
+
+    final = "/".join(
+        [f"{'=' if value == 3 else value}:{key}" for key, value in res.items()]
+    )
+
+    return final
