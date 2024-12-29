2a69ac57067ac66997c4fadc5b7946763985b411
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2024-12-29 19:46:42 +0100

    memorized fibonacci

---
+++ memo-fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(n, cache={}):
+    if n in cache:
+        return cache[n]
+    
+    if n <= 1:
+        return n
+    
+    cache[n] = fibonacci(n - 1, cache) + fibonacci(n - 2, cache)
+    
+    return cache[n]
